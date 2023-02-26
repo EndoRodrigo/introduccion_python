d860d723ff96d8d8dea317eec945e0778922c58d
EndoRodrigo <endorodrigo19@gmail.com>
2023-02-26 17:38:57 -0500

    Manejo de condicionales

---
+++ Condicionales.py
@@ -0,0 +1,26 @@
+# syntax
+'''if condition:
+    this part of code runs for truthy conditions'''
+
+a = -0
+if a > 0:
+    print('A is a positive number')
+elif a < 0:
+    print('A is a negative number')
+else:
+    print('A is zero')
+
+
+a = -3
+print('A is positive') if a > 0 else print('A is negative') # first condition met, 'A is positive' will be printed
+
+a = 4
+if a > 0:
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')
