b6bf984160db481e665d73118102cd5b4cff3f79
EndoRodrigo <endorodrigo19@gmail.com>
2023-02-26 15:28:25 -0500

    Mamenjo de tuplas

---
+++ Tuplas.py
@@ -0,0 +1,17 @@
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index =len(fruits) - 1
+#last_fruit = fruits[las_index]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+fruits = list(fruits)
+fruits[0] = 'apple'
+print(fruits)     # ['apple', 'orange', 'mango', 'lemon']
+fruits = tuple(fruits)
+print(fruits)     # ('apple', 'orange', 'mango', 'lemon')
