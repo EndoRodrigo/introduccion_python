196c40bea03f351bfaf97e266017ac0417270c46
EndoRodrigo <endorodrigo19@gmail.com>
2023-03-12 19:23:13 -0500

    Segundo ejercicio de programacion

---
+++ Ejercicios/Ejercico2.py
@@ -0,0 +1,10 @@
+#2. Programa que lea un nombre y muestre por pantalla:
+
+name = input('Ingrese su nombre-> ')
+print('El nombre ingresado es ->',name)
+
+#Solucion de chatgpt
+
+nombre = input("Ingrese su nombre: ")
+print("Su nombre es: ", nombre)
+
