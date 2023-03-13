672a8fedf52d83b0a9ad1ec997a2b64516c4b3f2
EndoRodrigo <endorodrigo19@gmail.com>
2023-03-12 19:19:03 -0500

    Primer ejecicio de programacion practico

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,14 @@
+# Programa que lea dos números enteros por teclado y 
+# los muestre por pantalla mi solucion
+
+numero_1 = int(input("Ingrese el primer numero entero-> "))
+numero_2 = int(input("Ingrese el segundo numero entero-> "))
+
+print(f'Los numeros ingresads son {numero_1} - {numero_2}')
+
+# La solucion de chatgpt
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+print("Los números ingresados son: ", num1, " y ", num2)
