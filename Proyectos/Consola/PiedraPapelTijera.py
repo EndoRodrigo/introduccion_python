6a9fee1e6ca80b2148cad157ca5fb0174d839c1d
EndoRodrigo <endorodrigo19@gmail.com>
2023-04-06 13:55:41 -0500

    Version 2 del juego piedra papel y tijera

--- Proyectos/Consola/PiedraPapelTijera.py
+++ Proyectos/Consola/PiedraPapelTijera.py
@@ -1,35 +1,42 @@
-#Creacion del juego piedra papel y tijera version 1
+#Creacion del juego piedra papel y tijera version 2
+import random
 
-user = int(input('''
-Selecione una opcion:
-    1. Piedra
-    2. Papel
-    3. Tijera
-    Respuest -> '''))
+while True:
+    user = int(input('''
+    Selecione una opcion:
+        1. Piedra
+        2. Papel
+        3. Tijera
+        Oprima cualquier tecla para finalizar el juego
+        Respuest -> '''))
 
-for computer in range(1,3):
-    continue
+    opciones = (1,2,3)
 
+    computer = random.randint(1, 3)
 
-if user == computer:
-    print(f"User -> {user} & computadora -> {computer} == EMPATE")
+    if not user in opciones: 
+        print('== END GAME ==')
+        break
 
-if user == 1 and computer == 2:
-    print(f"User -> {user} & computadora -> {computer} == GANANA LA COMPUTADORA")
+    if user == computer:
+        print(f"User -> {user} & computadora -> {computer} == EMPATE")
 
-if user == 1 and computer == 3:
-    print(f"User -> {user} & computadora -> {computer} == GANA EL USUARIO")
+    if user == 1 and computer == 2:
+        print(f"User -> {user} & computadora -> {computer} == GANANA LA COMPUTADORA")
 
-if user == 2 and computer == 1:
-    print(f"User -> {user} & computadora -> {computer} == GANANA LA COMPUTADORA")
+    if user == 1 and computer == 3:
+        print(f"User -> {user} & computadora -> {computer} == GANA EL USUARIO")
 
-if user == 2 and computer == 3:
-    print(f"User -> {user} & computadora -> {computer} == GANA EL USUARIO")
+    if user == 2 and computer == 1:
+        print(f"User -> {user} & computadora -> {computer} == GANANA LA COMPUTADORA")
 
-if user == 3 and computer == 1:
-    print(f"User -> {user} & computadora -> {computer} == GANANA LA COMPUTADORA")
+    if user == 2 and computer == 3:
+        print(f"User -> {user} & computadora -> {computer} == GANA EL USUARIO")
 
-if user == 3 and computer == 2:
-    print(f"User -> {user} & computadora -> {computer} == GANA EL USUARIO")
+    if user == 3 and computer == 1:
+        print(f"User -> {user} & computadora -> {computer} == GANANA LA COMPUTADORA")
+
+    if user == 3 and computer == 2:
+        print(f"User -> {user} & computadora -> {computer} == GANA EL USUARIO")
 
 
